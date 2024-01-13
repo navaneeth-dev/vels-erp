79df987df6f53affd9c145dc0225d45111acb3ef
Navaneeth Rao <decodestudios.help@gmail.com>
2024-01-13 19:35:52 +0530

    navbar

--- frontend/src/lib/components/main-nav.svelte
+++ frontend/src/lib/components/main-nav.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
     import {cn} from "$lib/utils";
+    import type {AuthModel} from "pocketbase";
 
     let className: string | undefined | null = undefined;
     export {className as class};
+    export let user: AuthModel | undefined;
 </script>
 
 <nav class={cn("flex items-center space-x-4 lg:space-x-6 container", className)}>
-    <a href="/" class="text-sm font-medium transition-colors hover:text-primary">
-        Dashboard
-    </a>
-    <a
-            href="/auth/register"
-            class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-        Register
-    </a>
-    <a
-            href="/auth/login"
-            class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-        Login
-    </a>
-    <a
-            href="/"
-            class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-        Settings
-    </a>
+    {#if user}
+        <a href="/" class="text-sm font-medium transition-colors hover:text-primary">
+            Dashboard
+        </a>
+        <a href="/leave" class="text-sm font-medium transition-colors hover:text-primary">
+            Apply For Leave
+        </a>
+    {:else}
+        <a
+                href="/auth/register"
+                class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        >
+            Register
+        </a>
+        <a
+                href="/auth/login"
+                class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        >
+            Login
+        </a>
+    {/if}
 </nav>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
     import '../app.pcss';
     import MainNav from '$lib/components/main-nav.svelte';
+
+    export let data;
+    const {user} = data;
 </script>
 
 <div class="hidden flex-col md:flex">
     <div class="border-b">
         <div class="flex h-16 items-center px-4">
-            <MainNav/>
+            <MainNav {user}/>
         </div>
     </div>
 </div>
