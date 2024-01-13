73279d5df9659bf1bd4e22b9b698294f37c7b35f
Navaneeth Rao <decodestudios.help@gmail.com>
2024-01-13 18:39:26 +0530

    formsnap

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
     import {Calendar} from "$lib/components/ui/calendar";
     import {Calendar as CalendarPrimitive} from "bits-ui";
-    import {today, getLocalTimeZone, DateFormatter} from "@internationalized/date";
+    import {DateFormatter, type DateValue, getLocalTimeZone, today} from "@internationalized/date";
+    import {Button} from "$lib/components/ui/button";
+    import * as Popover from "$lib/components/ui/popover";
+    import {cn} from "$lib/utils";
+    import {Calendar as CalendarIcon} from "radix-icons-svelte";
     import type {PageData} from "./$types";
 
-    let value = today(getLocalTimeZone());
+    const df = new DateFormatter("en-US", {
+        dateStyle: "long"
+    });
+
+    let value: DateValue = today(getLocalTimeZone());
 
     export let data: PageData;
     const workingDays = data.workingDays.map(day => new Date(day.date).getDay());
@@ -17,9 +25,26 @@
     <div class="">
         <h1 class="my-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Welcome
             Back, {data.user.name}</h1>
-        <div class="flex">
-            <Calendar bind:value class="border rounded-md shadow" isDateDisabled={isDateUnavailable}/>
-        </div>
+        <form method="post">
+            <Popover.Root>
+                <Popover.Trigger asChild let:builder>
+                    <Button
+                            variant="outline"
+                            class={cn(
+        "w-[240px] justify-start text-left font-normal",
+        !value && "text-muted-foreground"
+      )}
+                            builders={[builder]}
+                    >
+                        <CalendarIcon class="mr-2 h-4 w-4"/>
+                        {df.format(value.toDate(getLocalTimeZone()))}
+                    </Button>
+                </Popover.Trigger>
+                <Popover.Content class="w-auto p-0" align="start">
+                    <Calendar bind:value/>
+                </Popover.Content>
+            </Popover.Root>
+        </form>
     </div>
 {:else}
     <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">VELS ERP</h1>
