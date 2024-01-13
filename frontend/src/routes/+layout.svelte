0cda9d1fb3d9388fd43aa3381cebe24d3355e4ca
Navaneeth Rao <decodestudios.help@gmail.com>
2024-01-13 10:18:30 +0530

    shit

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-	import '../app.pcss';
+    import '../app.pcss';
+    import MainNav from '$lib/components/main-nav.svelte';
 </script>
 
-<slot />
+<div class="hidden flex-col md:flex">
+    <div class="border-b">
+        <div class="flex h-16 items-center px-4">
+            <MainNav/>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <slot/>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
+    // import {Icons} from "$lib/components/docs/icons";
     import {Button} from "$lib/components/ui/button";
+    import * as Card from "$lib/components/ui/card";
+    import {Label} from "$lib/components/ui/label";
+    import {Input} from "$lib/components/ui/input";
 </script>
 
-<div class="container space-y-3 flex flex-col">
-    <h1 class="text-3xl font-bold">Lorem ipsum dolor sit amet.</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque earum illum ipsum iusto maxime natus quas
-        soluta totam ut voluptas! Ad aliquam amet consectetur delectus dolores earum illum laboriosam laudantium minima
-        nemo nisi possimus sapiente, suscipit tempora unde. Ab autem consequatur delectus distinctio fugiat id ipsa
-        itaque iure labore mollitia natus necessitatibus neque nobis odio odit pariatur, porro quis, rem repellat
-        sapiente sed sit veritatis? A amet atque beatae dolores ea enim est eveniet explicabo facilis hic, iste minus
-        nemo nihil officia praesentium quae quam quasi quidem quo ratione similique tempora temporibus veritatis vero
-        voluptates. Earum excepturi quae sed tempore.</p>
-    <Button>Hello</Button>
-</div>
