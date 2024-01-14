671f213261ef5d0b2c6a81af2542b16c9c2b5263
Navaneeth Rao <decodestudios.help@gmail.com>
2024-01-14 12:32:59 +0530

    env var

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,8 +1,9 @@
 import PocketBase from 'pocketbase';
+import {POCKETBASE_URL} from "$env/static/private";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({event, resolve}) {
-    event.locals.pb = new PocketBase('http://127.0.0.1:8090');
+    event.locals.pb = new PocketBase(POCKETBASE_URL);
 
     // load the store data from the request cookie string
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
